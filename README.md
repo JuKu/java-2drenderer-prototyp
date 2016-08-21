897031f34e9ef441508a6bb8b2e0b33952e37f29
JuKu <kuenzel.justin@t-online.de>
2016-08-21 16:33:45 +0200

    added GLFWUtils to initialize and shutdown GLFW.

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ An Prototyp for an 2D renderer in Java 8 with OpenGL & LWJGL
 ## Requirements
   - Maven (you can also convert pom.xml files to gradle files)
   - LWJGL 3.0
+  - log4j
--- src/main/java/com/jukusoft/renderer2d/prototyp/Main.java
+++ src/main/java/com/jukusoft/renderer2d/prototyp/Main.java
@@ -1,12 +1,24 @@
 package com.jukusoft.renderer2d.prototyp;
 
+import com.jukusoft.renderer2d.prototyp.glfw.GLFWUtils;
+import org.apache.log4j.BasicConfigurator;
+
 /**
  * Created by Justin on 21.08.2016.
  */
 public class Main {
 
     public static void main (String[] args) {
-        //
+        //configure log4j to log to console
+        BasicConfigurator.configure();
+
+        //initialize GLFW
+        GLFWUtils.init();
+
+        //TODO: add code here
+
+        //shutdown GLFW
+        GLFWUtils.shutdownGLFW();
     }
 
 }
