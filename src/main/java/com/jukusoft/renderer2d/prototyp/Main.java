a286fc830f49429a57cd1b07743adcf3ee3e412f
JuKu <kuenzel.justin@t-online.de>
2016-08-21 16:10:42 +0200

    initial commit.

---
+++ src/main/java/com/jukusoft/renderer2d/prototyp/Main.java
@@ -0,0 +1,12 @@
+package com.jukusoft.renderer2d.prototyp;
+
+/**
+ * Created by Justin on 21.08.2016.
+ */
+public class Main {
+
+    public static void main (String[] args) {
+        //
+    }
+
+}
