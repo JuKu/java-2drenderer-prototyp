18f7688b5a748a693dd666cd107294f0ea6836fe
JuKu <kuenzel.justin@t-online.de>
2016-08-21 16:35:39 +0200

    change class Main.

--- src/main/java/com/jukusoft/renderer2d/prototyp/Main.java
+++ src/main/java/com/jukusoft/renderer2d/prototyp/Main.java
@@ -2,6 +2,7 @@ package com.jukusoft.renderer2d.prototyp;
 
 import com.jukusoft.renderer2d.prototyp.glfw.GLFWUtils;
 import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
 
 /**
  * Created by Justin on 21.08.2016.
@@ -12,10 +13,16 @@ public class Main {
         //configure log4j to log to console
         BasicConfigurator.configure();
 
+        //log message
+        Logger.getRootLogger().info("initialize GLFW now.");
+
         //initialize GLFW
         GLFWUtils.init();
 
         //TODO: add code here
+
+        //log message
+        Logger.getRootLogger().info("shutdown GLFW now.");
 
         //shutdown GLFW
         GLFWUtils.shutdownGLFW();
