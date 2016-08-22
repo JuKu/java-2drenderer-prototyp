3d11bc584bf55ef270b5572380071ea137f5c8f0
JuKu <kuenzel.justin@t-online.de>
2016-08-22 18:49:34 +0200

    added exception game state not found exception.

---
+++ prototyp-game-engine/src/main/java/com/jukusoft/renderer2d/prototyp/engine/exception/GameStateNotFoundException.java
@@ -0,0 +1,12 @@
+package com.jukusoft.renderer2d.prototyp.engine.exception;
+
+/**
+ * Created by Justin on 17.08.2016.
+ */
+public class GameStateNotFoundException extends Exception {
+
+    public GameStateNotFoundException(String message) {
+        super(message);
+    }
+
+}
