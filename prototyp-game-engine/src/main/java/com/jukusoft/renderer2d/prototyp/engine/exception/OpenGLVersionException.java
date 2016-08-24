46f4bd6bb6c2acd3ed8226da8b883f02e5c539cb
JuKu <kuenzel.justin@t-online.de>
2016-08-24 17:15:42 +0200

    added exception OpenGLVersionException, if OpenGL version isnt supported.

---
+++ prototyp-game-engine/src/main/java/com/jukusoft/renderer2d/prototyp/engine/exception/OpenGLVersionException.java
@@ -0,0 +1,12 @@
+package com.jukusoft.renderer2d.prototyp.engine.exception;
+
+/**
+ * Created by Justin on 24.08.2016.
+ */
+public class OpenGLVersionException extends Exception {
+
+    public OpenGLVersionException (String message) {
+        super(message);
+    }
+
+}
