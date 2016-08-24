6b9af0ac7aeb2b7d18b5929047d130ed9e2b7da5
JuKu <kuenzel.justin@t-online.de>
2016-08-24 20:12:42 +0200

    fixed some problems in vertex shader.

--- prototyp-game-engine/src/main/java/com/jukusoft/renderer2d/prototyp/engine/exception/ShaderException.java
+++ prototyp-game-engine/src/main/java/com/jukusoft/renderer2d/prototyp/engine/exception/ShaderException.java
@@ -3,7 +3,7 @@ package com.jukusoft.renderer2d.prototyp.engine.exception;
 /**
  * Created by Justin on 24.08.2016.
  */
-public class ShaderException extends Exception {
+public class ShaderException extends RuntimeException {
 
     public ShaderException (String message) {
         super(message);
--- protptyp-renderer-main/data/shader/vertex.vs
+++ protptyp-renderer-main/data/shader/vertex.vs
@@ -6,5 +6,5 @@ layout (location=0) in vec3 pos;
 void main()
 {
     //convert 3D vector to 4D vector, gl_Position is an output variable
-    gl_Position = vec4(position, 1.0);
+    gl_Position = vec4(pos, 1.0);
 }
