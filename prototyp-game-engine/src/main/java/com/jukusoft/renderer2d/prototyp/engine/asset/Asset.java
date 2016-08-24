d67f0f700a4c90ad31333cefec7139ddfb7ca55a
JuKu <kuenzel.justin@t-online.de>
2016-08-24 12:07:34 +0200

    added basic class for assets.

---
+++ prototyp-game-engine/src/main/java/com/jukusoft/renderer2d/prototyp/engine/asset/Asset.java
@@ -0,0 +1,70 @@
+package com.jukusoft.renderer2d.prototyp.engine.asset;
+
+/**
+ * an abstract class for assets to manage resources in resource manager / asset manager
+ *
+ * Created by Justin on 24.08.2016.
+ */
+public abstract class Asset {
+
+    /**
+    * reference counter
+     *
+     * on initial 1, because object was created
+    */
+    protected volatile int refCount = 1;
+
+    /**
+    * instance of resource manager / asset manager
+    */
+    protected ResourceManager resourceManager = null;
+
+    /**
+    * increment reference counter if asset is used by one more component / system
+    */
+    public void retain () {
+        this.refCount++;
+    }
+
+    /**
+    * decrement reference counter
+    */
+    public void release () {
+        this.refCount--;
+
+        //check, if asset can be removed
+        if (this.refCount <= 0) {
+            //notify listeners
+            this.notifyListener();
+
+            //cleanup asset
+            this.cleanUp();
+        }
+    }
+
+    /**
+    * get reference counter
+     *
+     * @return counter of objects / components / systems, which are using this asset
+    */
+    public int refCount () {
+        return this.refCount;
+    }
+
+    /**
+    * notify listener to remove asset
+    */
+    private void notifyListener () {
+        //check, if resource manager is set
+        if (this.resourceManager != null) {
+            //remove asset, because it isnt needed anymore
+            this.resourceManager.cleanUpAsset(this);
+        }
+    }
+
+    /**
+    * cleanUp asset
+    */
+    protected abstract void cleanUp ();
+
+}
---
+++ prototyp-game-engine/src/main/java/com/jukusoft/renderer2d/prototyp/engine/asset/ResourceManager.java
@@ -0,0 +1,15 @@
+package com.jukusoft.renderer2d.prototyp.engine.asset;
+
+/**
+ * Created by Justin on 24.08.2016.
+ */
+public interface ResourceManager {
+
+    /**
+    * cleanUp asset
+     *
+     * @param asset instance of asset
+    */
+    public void cleanUpAsset (Asset asset);
+
+}
