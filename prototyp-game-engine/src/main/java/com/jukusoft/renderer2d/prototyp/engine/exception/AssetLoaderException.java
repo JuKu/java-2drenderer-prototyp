703b031c41f1bae965c8511b44142087cab91d89
JuKu <kuenzel.justin@t-online.de>
2016-08-24 12:21:07 +0200

    added some exceptions.

---
+++ prototyp-game-engine/src/main/java/com/jukusoft/renderer2d/prototyp/engine/exception/AssetLoaderException.java
@@ -0,0 +1,14 @@
+package com.jukusoft.renderer2d.prototyp.engine.exception;
+
+/**
+ * Exception while loading asset, for example asset is broken
+ *
+ * Created by Justin on 28.02.2016.
+ */
+public class AssetLoaderException extends Exception {
+
+    public AssetLoaderException (String message) {
+        super(message);
+    }
+
+}
---
+++ prototyp-game-engine/src/main/java/com/jukusoft/renderer2d/prototyp/engine/exception/NoAssetLoaderFoundException.java
@@ -0,0 +1,12 @@
+package com.jukusoft.renderer2d.prototyp.engine.exception;
+
+/**
+ * Created by Justin on 27.02.2016.
+ */
+public class NoAssetLoaderFoundException extends Exception {
+
+    public NoAssetLoaderFoundException (String message) {
+        super(message);
+    }
+
+}
