e47275af86029991f26c76bcfbf748001a55cb29
JuKu <kuenzel.justin@t-online.de>
2016-08-24 14:44:31 +0200

    added exception ShaderException.

---
+++ prototyp-game-engine/src/main/java/com/jukusoft/renderer2d/prototyp/engine/exception/ShaderException.java
@@ -0,0 +1,12 @@
+package com.jukusoft.renderer2d.prototyp.engine.exception;
+
+/**
+ * Created by Justin on 24.08.2016.
+ */
+public class ShaderException extends Exception {
+
+    public ShaderException (String message) {
+        super(message);
+    }
+
+}
