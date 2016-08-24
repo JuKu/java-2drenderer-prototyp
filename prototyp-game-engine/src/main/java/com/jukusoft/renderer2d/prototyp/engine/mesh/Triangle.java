76eb02edf3eee37318cd8500e75ff311b1935d97
JuKu <kuenzel.justin@t-online.de>
2016-08-24 21:53:35 +0200

    added utils class MeshUtils to generate new meshIDs.

--- prototyp-game-engine/src/main/java/com/jukusoft/renderer2d/prototyp/engine/mesh/Triangle.java
+++ prototyp-game-engine/src/main/java/com/jukusoft/renderer2d/prototyp/engine/mesh/Triangle.java
@@ -1,6 +1,7 @@
 package com.jukusoft.renderer2d.prototyp.engine.mesh;
 
 import com.jukusoft.renderer2d.prototyp.engine.math.Vector3f;
+import com.jukusoft.renderer2d.prototyp.engine.utils.MeshUtils;
 
 /**
  * Created by Justin on 24.08.2016.
@@ -15,12 +16,20 @@ public class Triangle extends Mesh {
     protected Vector3f v3 = new Vector3f();
 
     /**
+    * local unique id of mesh
+    */
+    protected long meshID = 0;
+
+    /**
     * default constructor
     */
     public Triangle (Vector3f v1, Vector3f v2, Vector3f v3) {
         this.v1 = v1;
         this.v2 = v2;
         this.v3 = v3;
+
+        //generate new id of mesh
+        this.meshID = MeshUtils.generateID();
     }
 
     /**
@@ -30,13 +39,17 @@ public class Triangle extends Mesh {
         if (vertices.length != 9) {
             throw new IllegalArgumentException("length of array has to be 9, because 3x 3 vectors are required.");
         }
+
+        //generate new id of mesh
+        this.meshID = MeshUtils.generateID();
     }
 
     /**
      * default constructor
      */
     public Triangle () {
-        //
+        //generate new id of mesh
+        this.meshID = MeshUtils.generateID();
     }
 
     public float[] getVerticesArray () {
---
+++ prototyp-game-engine/src/main/java/com/jukusoft/renderer2d/prototyp/engine/utils/MeshUtils.java
@@ -0,0 +1,12 @@
+package com.jukusoft.renderer2d.prototyp.engine.utils;
+
+/**
+ * Created by Justin on 24.08.2016.
+ */
+public class MeshUtils {
+
+    public static long generateID () {
+        return LocalUniqueID.generateID();
+    }
+
+}
