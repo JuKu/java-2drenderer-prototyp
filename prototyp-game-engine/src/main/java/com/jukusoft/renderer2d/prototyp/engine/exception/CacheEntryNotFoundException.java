656528bc7478db60492604b1a70a3c0b7b5e12f9
JuKu <kuenzel.justin@t-online.de>
2016-08-24 15:11:00 +0200

    added exception CacheEntryNotFoundException.

---
+++ prototyp-game-engine/src/main/java/com/jukusoft/renderer2d/prototyp/engine/exception/CacheEntryNotFoundException.java
@@ -0,0 +1,12 @@
+package com.jukusoft.renderer2d.prototyp.engine.exception;
+
+/**
+ * Created by Justin on 24.08.2016.
+ */
+public class CacheEntryNotFoundException extends Exception {
+
+    public CacheEntryNotFoundException (String message) {
+        super(message);
+    }
+
+}
