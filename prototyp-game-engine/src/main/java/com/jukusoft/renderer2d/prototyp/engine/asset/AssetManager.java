494bd60cb3812b31c4cf1e928796ac11a2f59e22
JuKu <kuenzel.justin@t-online.de>
2016-08-24 12:16:30 +0200

    change class AssetManager, added support to add initializers.

--- prototyp-game-engine/src/main/java/com/jukusoft/renderer2d/prototyp/engine/asset/AssetManager.java
+++ prototyp-game-engine/src/main/java/com/jukusoft/renderer2d/prototyp/engine/asset/AssetManager.java
@@ -1,5 +1,8 @@
 package com.jukusoft.renderer2d.prototyp.engine.asset;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Asset manager to manages resources / assets
  *
@@ -13,6 +16,11 @@ public class AssetManager {
     protected static AssetManager instance = null;
 
     /**
+     * list with asset manager initializer to add asset loader
+     */
+    protected static List<AssetManagerInitializer> assetManagerInitializerList = new ArrayList<>();
+
+    /**
     * default constructor
     */
     public AssetManager () {
@@ -24,6 +32,9 @@ public class AssetManager {
         if (AssetManager.instance == null) {
             //create new asset manager
             AssetManager.instance = new AssetManager();
+
+            //initialize asset manager and add asset loaders
+            callInitializers();
         }
 
         //return instance of asset manager
@@ -38,6 +49,32 @@ public class AssetManager {
     public static void setInstance (AssetManager assetManager) {
         //override instance
         AssetManager.instance = assetManager;
+    }
+
+    /**
+    * add initializer for asset manager to add Assetloader
+     *
+     * @param initializer instance of asset manager initializer
+    */
+    public static void addInitializer (AssetManagerInitializer initializer) {
+        //check, if instance of asset manager already exists
+        if (instance != null) {
+            throw new IllegalStateException("AssetManager is already initialized, you have to add initializers before any call of getInstance().");
+        }
+
+        //add initializer to list
+        assetManagerInitializerList.add(initializer);
+    }
+
+    /**
+    * call asset manager initializers and add asset loaders
+    */
+    private static void callInitializers () {
+        //initialize asset manager
+        for (AssetManagerInitializer initializer : assetManagerInitializerList) {
+            //add asset loaders
+            initializer.initializeAssetLoader(instance);
+        }
     }
 
 }
