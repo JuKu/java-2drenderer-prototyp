76ae2d89236d5120a40bc2cb4dcb2f32e8ed5ca9
JuKu <kuenzel.justin@t-online.de>
2016-08-25 12:41:34 +0200

    change class GPUMesh, added support for AutoClosable.

--- prototyp-game-engine/src/main/java/com/jukusoft/renderer2d/prototyp/engine/mesh/GPUMesh.java
+++ prototyp-game-engine/src/main/java/com/jukusoft/renderer2d/prototyp/engine/mesh/GPUMesh.java
@@ -3,12 +3,17 @@ package com.jukusoft.renderer2d.prototyp.engine.mesh;
 /**
  * Created by Justin on 24.08.2016.
  */
-public class GPUMesh<T extends Mesh> {
+public abstract class GPUMesh<T extends Mesh> implements AutoCloseable {
 
     /**
     * instance of mesh
     */
     protected final T mesh;
+
+    /**
+    * flag if mesh was cleaned up
+    */
+    protected volatile boolean isCleanedUp = false;
 
     public GPUMesh(T mesh) {
         this.mesh = mesh;
@@ -22,13 +27,28 @@ public class GPUMesh<T extends Mesh> {
         return this.mesh;
     }
 
-    public void cleanUp () {
-        //
-    }
+    public abstract void init ();
+
+    public abstract void render ();
+
+    public abstract void cleanUp ();
 
     @Override
     public void finalize () {
-        this.cleanUp();
+        if (!this.isCleanedUp) {
+            this.cleanUp();
+
+            this.isCleanedUp = true;
+        }
+    }
+
+    @Override
+    public void close () throws Exception {
+        if (!this.isCleanedUp) {
+            this.cleanUp();
+
+            this.isCleanedUp = true;
+        }
     }
 
 }
