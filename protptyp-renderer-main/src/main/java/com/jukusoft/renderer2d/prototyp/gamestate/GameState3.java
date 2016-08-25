f987e71908f13f6c34d8e20f613f50594450595f
JuKu <kuenzel.justin@t-online.de>
2016-08-25 13:03:21 +0200

    change GameState3, added support to easy draw an triangle.

--- protptyp-renderer-main/src/main/java/com/jukusoft/renderer2d/prototyp/gamestate/GameState3.java
+++ protptyp-renderer-main/src/main/java/com/jukusoft/renderer2d/prototyp/gamestate/GameState3.java
@@ -4,6 +4,9 @@ import com.jukusoft.renderer2d.prototyp.engine.app.GameApp;
 import com.jukusoft.renderer2d.prototyp.engine.gamestate.GameState;
 import com.jukusoft.renderer2d.prototyp.engine.gamestate.GameStateManager;
 import com.jukusoft.renderer2d.prototyp.engine.gamestate.impl.BasicGameState;
+import com.jukusoft.renderer2d.prototyp.engine.math.Vector3f;
+import com.jukusoft.renderer2d.prototyp.engine.mesh.data.GPUTriangle;
+import com.jukusoft.renderer2d.prototyp.engine.mesh.data.Triangle;
 import com.jukusoft.renderer2d.prototyp.engine.opengl.buffer.FloatVertexBufferObject;
 import com.jukusoft.renderer2d.prototyp.engine.shader.OpenGLShaderProgram;
 import com.jukusoft.renderer2d.prototyp.engine.utils.FileUtils;
@@ -20,9 +23,43 @@ import static org.lwjgl.opengl.GL11.glDrawArrays;
  */
 public class GameState3 extends BasicGameState {
 
+    /**
+    * triangle on gpu
+    */
+    protected GPUTriangle gpuTriangle = null;
+
+    /**
+     * instance of shader program
+     */
+    protected OpenGLShaderProgram shaderProgram = null;
+
     @Override
     public <T extends GameState> void onInit(GameStateManager<T> gameStateManager, GameApp app) {
         Logger.getRootLogger().info("GameState3::onInit().");
+
+        //create new shader program
+        this.shaderProgram = new OpenGLShaderProgram();
+
+        try {
+            //add vertex shader
+            this.shaderProgram.setVertexShader(FileUtils.readFile("./data/shader/vertex.vs", StandardCharsets.UTF_8));
+
+            //add fragment shader
+            this.shaderProgram.setFragmentShader(FileUtils.readFile("./data/shader/fragment.fs", StandardCharsets.UTF_8));
+
+            //link shader program
+            this.shaderProgram.link();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        //create new triangle
+        Triangle triangle = new Triangle(new Vector3f(0.0f, 0.5f, 0.0f), new Vector3f(-0.5f, -0.5f, 0.0f), new Vector3f(0.5f, -0.5f, 0.0f));
+        this.gpuTriangle = new GPUTriangle(triangle);
+
+        //initialize triangle on gpu
+        this.gpuTriangle.init();
 
         getWindow().setClearColor(0, 0, 0, 0);
     }
@@ -36,7 +73,14 @@ public class GameState3 extends BasicGameState {
             getWindow().setResizedFlag(false);
         }
 
-        //TODO: render
+        //bind shader program
+        this.shaderProgram.bind();
+
+        //render triangle
+        this.gpuTriangle.render();
+
+        //unbind shader program
+        this.shaderProgram.unbind();
     }
 
 }
