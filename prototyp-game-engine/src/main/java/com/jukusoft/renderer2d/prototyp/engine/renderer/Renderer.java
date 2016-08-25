8d1a1e2d0d12182f4af2e06394b06eb5966ab6a0
JuKu <kuenzel.justin@t-online.de>
2016-08-25 12:52:47 +0200

    added interface Renderer.

---
+++ prototyp-game-engine/src/main/java/com/jukusoft/renderer2d/prototyp/engine/renderer/Renderer.java
@@ -0,0 +1,18 @@
+package com.jukusoft.renderer2d.prototyp.engine.renderer;
+
+/**
+ * Created by Justin on 25.08.2016.
+ */
+public interface Renderer {
+
+    /**
+    * initialize renderer
+    */
+    public void init ();
+
+    /**
+    * render
+    */
+    public void render ();
+
+}
